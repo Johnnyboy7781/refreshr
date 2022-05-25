b1a795305b94b69da74792b83765b13bb3c8ff79
Lily Kirsche <lkirsche01@gmail.com>
2022-05-24 20:05:51 -0400

    set up auth

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    cart: [Drink]!
+    favorites: [Drink]!
+  }
+
+  type Drink {
+    _id: ID
+    name: String
+    description: String
+    price: Number
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]
+    user(username: String!): User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+  }
+`;
+
+module.exports = typeDefs;
