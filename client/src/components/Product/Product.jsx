11782af025fd22f1f9eee19656e8337b81755429
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-05-31 16:41:11 -0400

    styled product page
    added styled components and material ui icons to the products page

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,16 +1,90 @@
-import React from "react";
-import "./Product.css";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import InfoIcon from '@mui/icons-material/Info';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-color: rgba(0, 0, 0, 0.2);
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.5s ease;
+  cursor: pointer;
+`;
+
+const Container = styled.div`
+  flex: 1;
+  margin: 5px;
+  min-width: 280px;
+  height: 350px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #c2f8fa;
+  position: relative;
+
+  &:hover ${Info}{
+    opacity: 1;
+  }
+`;
+
+const Circle = styled.div`
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  background-color: white;
+  position: absolute;
+`;
+
+const Image = styled.img`
+  height: 75%;
+  z-index: 2;
+`;
+
+const Icon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 10px;
+  transition: all 0.5s ease;
+  &:hover {
+    background-color: #e9f5f5;
+    transform: scale(1.75);
+  }
+`;
 
 const Product = ({ drink }) => {
-    return (
-        <div className="container">
-            <a href={`/drink/${drink._id}`}>
-                <img src={require(`../../assets/${drink.image}.png`)} alt="A drink" className="productImg" />
-            </a>
-            <h3>{drink.name}</h3>
-            <h4>${drink.price}</h4>
-        </div>
-    )
-}
+  return (
+    <Container>
+      <Circle />
+      <Image src={require(`../../assets/${drink.image}.png`)} alt="A drink" />
+      <Info>
+        <Icon>
+          <AddShoppingCartIcon />
+        </Icon>
+        <Icon>
+          <Link to={`${drink._id}`}>
+          <InfoIcon />
+          </Link>
+        </Icon>
+        <Icon>
+          <FavoriteBorderIcon />
+        </Icon>
+      </Info>
+    </Container>
+  );
+};
 
 export default Product;
