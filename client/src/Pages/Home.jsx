2f0f2c92027d9c185fb73b978f93a596f677d01b
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-05-31 19:45:26 -0400

    Footer
    added footer with simple styling

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 import Navbar from "../components/Navbar/Navbar";
 import Products from "../components/Products/Products";
+import Footer from "../components/Footer/Footer";
+
+
 
 
 
@@ -10,6 +13,8 @@ const Home = () => {
         <div>
             <Navbar />
             <Products />
+            <Footer />
+
         </div>
     )
 }
