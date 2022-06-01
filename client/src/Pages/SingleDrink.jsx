387c7e2f6f88278730f3037adaf7c1984bc0b8ae
JaredTempleton <95387966+JaredTempleton@users.noreply.github.com>
2022-06-01 00:39:17 -0400

    added single drink styling
    also fixed few typos in register.jsx and cleaned up some css in products.css

--- client/src/Pages/SingleDrink.jsx
+++ client/src/Pages/SingleDrink.jsx
@@ -1,9 +1,95 @@
-import React from "react";
+import { React, useState } from "react";
+import styled from "styled-components";
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+import Navbar from "../components/Navbar/Navbar";
 
 import { useParams } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 
 import { QUERY_DRINK } from "../utils/queries";
+
+const Container = styled.div`
+  background:
+    url("https://img.freepik.com/free-photo/aluminum-cans-soda-background_128406-587.jpg?w=1380")
+      center;
+  background-size: cover;`;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+  border: solid 5px teal;
+  border-radius: 5px;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+  width: 25%;
+  height: auto;
+  background-color: white;
+  border: solid 5px teal;
+  border-radius: 5px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover {
+    background-color: #f8f4f4;
+  }
+`;
 
 const SingleDrink = () => {
     const { drinkId } = useParams();
@@ -15,21 +101,46 @@ const SingleDrink = () => {
     });
 
     const drink = data?.drink || {};
+    
+    const [quantity, setQuantity] = useState(1);
+
+    const handleQuantity = (type) => {
+        if (type === "dec") {
+          quantity > 1 && setQuantity(quantity - 1);
+        } else {
+          setQuantity(quantity + 1);
+        }
+      };
+    const handleClick = () => {
+
+    }
 
     if(loading) {
         return <div>Loading...</div>;
     }
     return (
-        <section className="drink-page">
-            <h3>{drink.name}</h3>
-            <h4>${drink.price}</h4>
-            <img 
-                src={require(`../assets/${drink.image}.png`)}
-                alt="A drink"
-            />
-            <p>{drink.description}</p>
-        </section>
-    );
+        <Container>
+            <Navbar />
+      <Wrapper>
+        <ImgContainer>
+          <Image src={require(`../assets/${drink.image}.png`)} alt="A drink" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>{drink.name}</Title>
+          <Desc>{drink.description}</Desc>
+          <Price>$ {drink.price}</Price>
+          <AddContainer>
+            <AmountContainer>
+              <RemoveIcon onClick={() => handleQuantity("dec")} />
+              <Amount>{quantity}</Amount>
+              <AddIcon onClick={() => handleQuantity("inc")}/>
+            </AmountContainer>
+            <Button onClick={handleClick}>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default SingleDrink;
