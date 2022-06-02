6884155205b1f9bd7eec12171ee25236143662f9
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-06-02 19:05:15 -0400

    Update cart page

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const Stripe = require('stripe');
-const stripe = new Stripe(process.env.SECRET_KEY);
 
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require("./utils/auth");
@@ -10,6 +8,11 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+require('dotenv').config();
+const stripe = require('stripe')(process.env.REACT_APP_SECRET_KEY);
+
+console.log("Key",process.env.REACT_APP_SECRET_KEY);
 
 let server;
 
@@ -35,6 +38,22 @@ if (process.env.NODE_ENV === 'production') {
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 // });
+
+app.post('/api/payment_intents', async (req, res) => {
+  try {
+    const { amount } = req.body;
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: "usd",
+      automatic_payment_methods: {
+        enabled: true
+      }
+    })
+    res.status(200).send(paymentIntent.client_secret);
+  } catch (err) {
+    res.status(404).json({ message: err.message })
+  }
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
